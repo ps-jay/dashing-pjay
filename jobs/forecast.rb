4a4d0b7c010572731790baff5bedf133cf331765
Philip Jay <phil@jay.id.au>
2015-06-05 15:41:08 +1000

    Initial commit

---
+++ jobs/forecast.rb
@@ -0,0 +1,26 @@
+require 'net/https'
+require 'json'
+
+# Forecast API Key from https://developer.forecast.io
+forecast_api_key = "3c76d37bc79af5213db4908f326d2906"
+
+# Latitude, Longitude for location
+forecast_location_lat = "-37.8260"
+forecast_location_long = "145.1510"
+
+# Unit Format
+# "us" - U.S. Imperial
+# "si" - International System of Units
+# "uk" - SI w. windSpeed in mph
+forecast_units = "si"
+  
+SCHEDULER.every '10m', :first_in => 0 do |job|
+  http = Net::HTTP.new("api.forecast.io", 443)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
+  forecast = JSON.parse(response.body)  
+  forecast_current_temp = forecast["currently"]["temperature"].round
+  forecast_hour_summary = forecast["hourly"]["summary"]
+  send_event('forecast', { temperature: "#{forecast_current_temp}&deg;", hour: "#{forecast_hour_summary}"})
+end

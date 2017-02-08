70593c06fe2dd480f798ff2af5a5aa8b34648af9
Philip Jay <phil@jay.id.au>
2017-02-08 23:23:37 +1100

    Fix for change in BOM data
    Using precipitation_range was unreliable.  Figured I should learn
    iso8601 parsing in Ruby.  It wasn't so hard.

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -1,46 +1,60 @@
 require 'net/http'
+require 'time'
 require 'uri'
 require 'nokogiri'
 
 bom_area = 'VIC_PT042'
 uri = URI.parse('http://www.bom.gov.au/fwo/IDV10753.xml')
 
-dirname = File.dirname(uri.path)
-basename = File.basename(uri.path)
-
 SCHEDULER.every '60m', :first_in => 0 do |job|
   http = Net::HTTP.new(uri.host, uri.port)
   resp = http.request(Net::HTTP::Get.new(uri.request_uri))
   forecast = Nokogiri::XML(resp.body)
 
-  if forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='0']/element[@type='precipitation_range']").empty? then
-    day_1 = {
-      'name' => 'Tomorrow',
-      'index' => 1,
-    }
-    t_plus_2 = Time.now + 2 * 24 * 3600
-    day_2 = {
-      'name' => t_plus_2.strftime("%A"),
-      'index' => 2,
-    }
+  end_time_index_0 = Time.iso8601(forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='0']").first.attributes['end-time-local'].value)
+
+  if Time.now < end_time_index_0 then
+    if Time.now.hour < 16 then
+      # Forecast was issued today, and it's before 4pm - show today and tomorrow
+      day_1 = {
+        'name' => 'Today',
+        'index' => 0,
+      }
+      day_2 = {
+        'name' => 'Tomorrow',
+        'index' => 1,
+      }
+    else
+      # Forecast was issued today, and it's after 4pm - show tomorrow and the next day
+      day_1 = {
+        'name' => 'Tomorrow',
+        'index' => 1,
+      }
+      t_plus_2 = Time.now + 2 * 24 * 3600
+      day_2 = {
+        'name' => t_plus_2.strftime("%A"),
+        'index' => 2,
+      }
+    end
   else
+    # Forecast was issued yesterday - show today and tomorrow
     day_1 = {
       'name' => 'Today',
-      'index' => 0,
+      'index' => 1,
     }
     day_2 = {
       'name' => 'Tomorrow',
-      'index' => 1,
+      'index' => 2,
     }
   end
 
-  day_1['min'] =  forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_1['index']}']/element[@type='air_temperature_minimum']").first.children.first.text
+  day_1['min'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_1['index']}']/element[@type='air_temperature_minimum']").first.children.first.text
   day_1['max'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_1['index']}']/element[@type='air_temperature_maximum']").first.children.first.text
   day_1['rain_chance'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_1['index']}']/text[@type='probability_of_precipitation']").first.children.first.text
   day_1['rain_range'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_1['index']}']/element[@type='precipitation_range']").first.children.first.text
   day_1['precis'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_1['index']}']/text[@type='precis']").first.children.first.text
 
-  day_2['min'] =  forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_2['index']}']/element[@type='air_temperature_minimum']").first.children.first.text
+  day_2['min'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_2['index']}']/element[@type='air_temperature_minimum']").first.children.first.text
   day_2['max'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_2['index']}']/element[@type='air_temperature_maximum']").first.children.first.text
   day_2['rain_chance'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_2['index']}']/text[@type='probability_of_precipitation']").first.children.first.text
   day_2['rain_range'] = forecast.xpath("//area[@aac='#{bom_area}']/forecast-period[@index='#{day_2['index']}']/element[@type='precipitation_range']").first.children.first.text

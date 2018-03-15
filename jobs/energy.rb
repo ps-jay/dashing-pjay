32c3b5a218b57f11c0e45d5141af7ea33421dd31
Philip Jay <phil@jay.id.au>
2018-03-15 22:43:31 +1100

    Work in progres for Laurel

--- jobs/energy.rb
+++ jobs/energy.rb
@@ -1,84 +1,51 @@
-require 'sqlite3'
-
-curr_meter_time = 0
-curr_meter_read = 0
-
-curr_solar_time = 0
-curr_solar_read = 0
+meter = 0
+solar = 0
+meter_update = false
+solar_update = false
 
 curr_use = 0
 
-SCHEDULER.every '15s', :first_in => 0 do |job|
-
-  last_meter_time = curr_meter_time
-  last_meter_read = curr_meter_read
-
-  last_solar_time = curr_solar_time
-  last_solar_read = curr_solar_read
-
-  last_use = curr_use
-  time_change = false
+SCHEDULER.every '30s', :first_in => 0 do |job|
 
   # Get metered demand
   begin
-    db = SQLite3::Database.open "/energy-data/raven.sqlite"
-    db.results_as_hash = true
-    stm = db.prepare "SELECT * FROM demand ORDER BY timestamp DESC LIMIT 1"
-    rs = stm.execute
-    rs.each do |row|
-      curr_meter_time = row['timestamp']
-      curr_meter_read = row['watts'] / 1000.0
-    end
-  ensure
-    stm.close if stm
-    db.close if db
+     new_meter_read = `python jobs/rain_eagle.py`
+     if new_meter_read != '' then
+         meter = new_meter_read.to_i
+         meter_update = true
+     end
   end
 
   # Get solar generation
   begin
-    db = SQLite3::Database.open "/energy-data/solar.sqlite"
-    db.results_as_hash = true
-    stm = db.prepare "SELECT * FROM system ORDER BY timestamp DESC LIMIT 1"
-    rs = stm.execute
-    rs.each do |row|
-      curr_solar_time = row['timestamp']
-      curr_solar_read = row['pout_W'] / 1000.0
-    end
-  ensure
-    stm.close if stm
-    db.close if db
+     new_solar_read = 123
+     if new_solar_read != '' then
+         solar = new_solar_read
+         solar_update = true
+     end
   end
 
   # Calculate consumption
-  curr_use = curr_meter_read + curr_solar_read
-  if curr_use < 0.3
-    curr_use = 0.3  # Floor value
+  curr_use = meter + solar
+  if curr_use < 200
+    curr_use = 200  # Floor value
   end
 
   # Rounding
-  curr_meter_read = ("%0.01f" % curr_meter_read).to_f()
-  curr_solar_read = ("%0.01f" % curr_solar_read).to_f()
-  curr_use = ("%0.01f" % curr_use).to_f()
+  meter = ("%0.01f" % (meter / 1000.0)).to_f()
+  solar = ("%0.01f" % (solar / 1000.0)).to_f()
+  curr_use = ("%0.01f" % (curr_use / 1000.0)).to_f()
 
-  if curr_meter_time != last_meter_time
-    if curr_meter_read < 0
-      title = "Selling"
-      value = curr_meter_read * -1
-    else
-      title = "Buying"
-      value = curr_meter_read
-    end
-    send_event('griddemand', { value: value, title: title })
-    time_change = true
+  if solar_update then
+    send_event('generating', { value: solar })
+    solar_update = false
   end
 
-  if curr_solar_time != last_solar_time
-    send_event('generating', { value: curr_solar_read })
-    time_change = true
-  end
-
-  if time_change
+  if meter_update or solar_update then
     send_event('consuming', { value: curr_use })
   end
+
+  meter_update = false
+  solar_update = false
 
 end

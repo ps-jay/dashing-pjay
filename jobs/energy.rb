a4e53880e9e2be44857da33ba96f15adcde0c061
Philip Jay <phil@jay.id.au>
2018-03-18 22:54:50 +1100

    Update for # of panels

--- jobs/energy.rb
+++ jobs/energy.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 meter = 0
 solar = 0
 meter_update = false
@@ -9,20 +12,41 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
 
   # Get metered demand
   begin
-     new_meter_read = `python jobs/rain_eagle.py`
-     if new_meter_read != '' then
-         meter = new_meter_read.to_i
-         meter_update = true
-     end
+    new_meter_read = `python jobs/rain_eagle.py`
+    if new_meter_read != '' then
+      meter = new_meter_read.to_i
+      meter_update = true
+    end
   end
 
   # Get solar generation
   begin
-     new_solar_read = 123
-     if new_solar_read != '' then
-         solar = new_solar_read
-         solar_update = true
-     end
+    uri = URI.parse('http://10.10.8.42/home')
+    http = Net::HTTP.new(uri.host, uri.port)
+    resp = http.request(Net::HTTP::Get.new(uri.request_uri))
+
+    generate_start = /<td>Currently generating<\/td>/ =~ resp.body
+    generate_to_eof = resp.body[generate_start..-1]
+    generate_kw = /<td>\s*\d+\sk?W\s*<\/td>/ =~ generate_to_eof
+    generate_end = /\d\s/ =~ generate_to_eof[(generate_kw+4)..-1]
+    new_solar_read = generate_to_eof[(generate_kw+4),(generate_end+1)].strip
+
+    panels_start = /<td>Number of Microinverters<\/td>/ =~ resp.body
+    panels_to_eof = resp.body[panels_start..-1]
+    panels_count = /<td>\d+<\/td>/ =~ panels_to_eof
+    panels_end = /<\/td>/ =~ panels_to_eof[(panels_count+4)..-1]
+    panels = panels_to_eof[(panels_count+4),panels_end]
+
+    online_start = /<td>Number of Microinverters Online<\/td>/ =~ resp.body
+    online_to_eof = resp.body[online_start..-1]
+    online_count = /<td>\d+<\/td>/ =~ online_to_eof
+    online_end = /<\/td>/ =~ online_to_eof[(online_count+4)..-1]
+    online = online_to_eof[(online_count+4),online_end]
+
+    if new_solar_read != '' then
+      solar = new_solar_read
+      solar_update = true
+    end
   end
 
   # Calculate consumption
@@ -37,7 +61,7 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
   curr_use = ("%0.01f" % (curr_use / 1000.0)).to_f()
 
   if solar_update then
-    send_event('generating', { value: solar })
+    send_event('generating', { value: solar, panels: panels, online: online })
     solar_update = false
   end
 

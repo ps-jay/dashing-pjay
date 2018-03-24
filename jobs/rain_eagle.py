616172a37b1c79574f21700c2d02e69d0bb9abee
Philip Jay <phil@jay.id.au>
2018-03-24 06:32:27 +0000

    Add code to support negative numbers

--- jobs/rain_eagle.py
+++ jobs/rain_eagle.py
@@ -13,9 +13,27 @@ try:
 except:
     pass
 
+def _undo_twos(str_value, num_digits=None):
+    '''Convert a twos complement hex string to a signed int'''
+    if num_digits is not None:
+        digits = len(str_value) - 2
+    else:
+        digits = num_digits
+    pattern8 = '0x8'
+    patternF = '0xF'
+    for i in range(1, digits):
+        pattern8 += '0'
+        patternF += 'F'
+    if int(str_value, 16) < int(pattern8, 16):
+        n = int(str_value, 16)
+    else:
+        n = -1 * int(patternF, 16) + int(str_value, 16) - 1
+    return n
+
 try:
     raineagle = RainEagle.Eagle(debug=0, addr='10.10.8.41', username=USERNAME, password=PASSWORD)
 
-    print(int(raineagle.get_instantaneous_demand()['InstantaneousDemand']['Demand'], 0))
+    reading = raineagle.get_instantaneous_demand()['InstantaneousDemand']['Demand']
+    print(_undo_twos(reading, num_digits=8))
 except:
     pass
